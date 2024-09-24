53f5c4e92c42f51e2fd10f59ce2ce24e17239505
bhargav1262 <72906578+bhargav1262@users.noreply.github.com>
2024-09-24 02:52:03 +0000

    class02

---
+++ 01_introduction/b_hello_world.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+print('Hello world')
+
+
+# print as a statement in python 2
+# print "Hello world"
+
+# print as a function in python 2 & 3
+
+print(True)
+print(True, 123, None)
+
+def wishes(name):
+    wish = "How are you {0}?".format(name)
+    print(wish)
+
+wishes("Bhargav")
---
+++ 01_introduction/d_indentation_issue.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print("Hello world")
+
+if 12 > 3:
+    print("print 12 is greater than 3")
